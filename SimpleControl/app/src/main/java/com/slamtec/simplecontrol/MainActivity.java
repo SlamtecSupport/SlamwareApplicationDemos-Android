7fad272e9145c5e843f66364644a3db3ab72d5e9
Forrest Gan <forrest.gan@slamtec.com>
2018-08-28 16:03:35 +0800

    修改SimpleControl的按键监控函数

--- SimpleControl/app/src/main/java/com/slamtec/simplecontrol/MainActivity.java
+++ SimpleControl/app/src/main/java/com/slamtec/simplecontrol/MainActivity.java
@@ -9,101 +9,92 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.slamtec.slamware.AbstractSlamwarePlatform;
+import com.slamtec.slamware.action.IMoveAction;
 import com.slamtec.slamware.action.MoveDirection;
 import com.slamtec.slamware.discovery.DeviceManager;
+import com.slamtec.slamware.exceptions.ConnectionFailException;
+import com.slamtec.slamware.exceptions.ConnectionTimeOutException;
+import com.slamtec.slamware.exceptions.InvalidArgumentException;
+import com.slamtec.slamware.exceptions.OperationFailException;
+import com.slamtec.slamware.exceptions.ParseInvalidException;
+import com.slamtec.slamware.exceptions.RequestFailException;
+import com.slamtec.slamware.exceptions.UnauthorizedRequestException;
+import com.slamtec.slamware.exceptions.UnsupportedCommandException;
 import com.slamtec.slamware.robot.Pose;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
-    private Bundle savedInstanceState;
-    private String deviceId;
-    private Pose pose;
+    private AbstractSlamwarePlatform robotPlatform;
+    private IMoveAction moveAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        this.savedInstanceState = savedInstanceState;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final AbstractSlamwarePlatform robotPlatform = DeviceManager.connect("192.168.11.1", 1445);
-
+        /* 连接底盘 */
+        robotPlatform = DeviceManager.connect("10.0.130.71", 1445);
         if(robotPlatform != null) {
-
             Log.d(TAG, "Slamware connect success");
-
-            try {
-                Pose pose = robotPlatform.getPose();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            try {
-                deviceId = robotPlatform.getDeviceId();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
         }
 
-        System.out.println("deviceId = " + deviceId);
-
-
-        // go forward
+        Button button_stop = (Button) findViewById(R.id.button_stop);
         Button button_forward = (Button) findViewById(R.id.button_forward);
-        button_forward.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    robotPlatform.moveBy(MoveDirection.FORWARD);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        // go backward
         Button button_backward = (Button) findViewById(R.id.button_backward);
-        button_backward.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    robotPlatform.moveBy(MoveDirection.BACKWARD);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        // turn left
         Button button_turn_left = (Button) findViewById(R.id.button_turn_left);
-        button_turn_left.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    robotPlatform.moveBy(MoveDirection.TURN_LEFT);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
-
-        // turn right
         Button button_turn_right = (Button) findViewById(R.id.button_turn_right);
-        button_turn_right.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    robotPlatform.moveBy(MoveDirection.TURN_RIGHT);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
 
-
+        button_stop.setOnClickListener(this);
+        button_forward.setOnClickListener(this);
+        button_backward.setOnClickListener(this);
+        button_turn_left.setOnClickListener(this);
+        button_turn_right.setOnClickListener(this);
+        button_backward.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
 
+        try{
+            switch (view.getId()) {
+
+                case R.id.button_stop:
+                    if(moveAction != null) {
+                        moveAction.cancel();
+                    }
+                    break;
+                case R.id.button_forward:
+                    moveAction = robotPlatform.moveBy(MoveDirection.FORWARD);
+                    break;
+                case R.id.button_backward:
+                    moveAction = robotPlatform.moveBy(MoveDirection.BACKWARD);
+                    break;
+                case R.id.button_turn_left:
+                    moveAction = robotPlatform.moveBy(MoveDirection.TURN_LEFT);
+                    break;
+                case R.id.button_turn_right:
+                    moveAction = robotPlatform.moveBy(MoveDirection.TURN_RIGHT);
+                    break;
+                default:
+                    break;
+            }
+        } catch (RequestFailException e) {
+            e.printStackTrace();
+        } catch (ConnectionFailException e) {
+            e.printStackTrace();
+        } catch (ConnectionTimeOutException e) {
+            e.printStackTrace();
+        } catch (UnauthorizedRequestException e) {
+            e.printStackTrace();
+        } catch (UnsupportedCommandException e) {
+            e.printStackTrace();
+        } catch (ParseInvalidException e) {
+            e.printStackTrace();
+        } catch (InvalidArgumentException e) {
+            e.printStackTrace();
+        } catch (OperationFailException e) {
+            e.printStackTrace();
+        }
+    }
 }
