59571de639d52381b976d179f34e30837e1ac0f2
Forrest Gan <forrest.gan@slamtec.com>
2018-08-29 14:14:44 +0800

    midified RotationAction

--- RotationAction/app/src/main/java/com/slamtec/rotationaction/MainActivity.java
+++ RotationAction/app/src/main/java/com/slamtec/rotationaction/MainActivity.java
@@ -21,81 +21,78 @@ import com.slamtec.slamware.exceptions.UnsupportedCommandException;
 import com.slamtec.slamware.robot.MoveOption;
 import com.slamtec.slamware.robot.Rotation;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
 
     private IMoveAction action;
+    private AbstractSlamwarePlatform robotPlatform;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final AbstractSlamwarePlatform robotPlatform = DeviceManager.connect("192.168.11.1", 1445);
+        robotPlatform = DeviceManager.connect("10.0.130.71", 1445);
 
         Button buttonClockwise = (Button) findViewById(R.id.button_clockwise);
-
         Button buttonAnticlockwise = (Button) findViewById(R.id.button_anticlockwise);
-
-        buttonClockwise.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    action = robotPlatform.rotate(new Rotation(setPI(90), 0, 0));
-                //    action.waitUntilDone();
-                    Log.d(TAG, "Clockwise Rotate 90");
-                } catch (RequestFailException e) {
-                    e.printStackTrace();
-                } catch (ConnectionFailException e) {
-                    e.printStackTrace();
-                } catch (ConnectionTimeOutException e) {
-                    e.printStackTrace();
-                } catch (UnauthorizedRequestException e) {
-                    e.printStackTrace();
-                } catch (UnsupportedCommandException e) {
-                    e.printStackTrace();
-                } catch (ParseInvalidException e) {
-                    e.printStackTrace();
-                } catch (InvalidArgumentException e) {
-                    e.printStackTrace();
-                } catch (OperationFailException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        buttonAnticlockwise.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    action = robotPlatform.rotate(new Rotation(setPI(-90), 0, 0));
-                //    action.waitUntilDone();
-                    Log.d(TAG, "Anticlockwise Rotate 90");
-                } catch (RequestFailException e) {
-                    e.printStackTrace();
-                } catch (ConnectionFailException e) {
-                    e.printStackTrace();
-                } catch (ConnectionTimeOutException e) {
-                    e.printStackTrace();
-                } catch (UnauthorizedRequestException e) {
-                    e.printStackTrace();
-                } catch (UnsupportedCommandException e) {
-                    e.printStackTrace();
-                } catch (ParseInvalidException e) {
-                    e.printStackTrace();
-                } catch (InvalidArgumentException e) {
-                    e.printStackTrace();
-                } catch (OperationFailException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
+        Button buttonRotateToYaw = (Button) findViewById(R.id.button_rotate_to);
+        buttonClockwise.setOnClickListener(this);
+        buttonAnticlockwise.setOnClickListener(this);
+        buttonRotateToYaw.setOnClickListener(this);
 
     }
 
     private float setPI(int i) {
         return (float)(i*Math.PI/180);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        try {
+            /* 如果当前正在运动，先停止 */
+            if(action != null) {
+                action.cancel();
+            }
+
+            switch (view.getId()) {
+                /* Anticlockwise Rotate 180 */
+                case R.id.button_anticlockwise:
+                    action = robotPlatform.rotate(new Rotation(setPI(-180), 0, 0));
+                    Log.d(TAG, "");
+                    break;
+
+                /* Clockwise Rotate 180 */
+                case R.id.button_clockwise:
+                    action = robotPlatform.rotate(new Rotation(setPI(180), 0, 0));
+                    break;
+
+                /* Rotate to 270 */
+                case R.id.button_rotate_to:
+                    action = robotPlatform.rotateTo(new Rotation(setPI(270), 0, 0));
+                    break;
+
+                default:
+                    break;
+            }
+        } catch (RequestFailException e) {
+            e.printStackTrace();
+        } catch (ConnectionFailException e) {
+            e.printStackTrace();
+        } catch (ConnectionTimeOutException e) {
+            e.printStackTrace();
+        } catch (UnauthorizedRequestException e) {
+            e.printStackTrace();
+        } catch (UnsupportedCommandException e) {
+            e.printStackTrace();
+        } catch (ParseInvalidException e) {
+            e.printStackTrace();
+        } catch (InvalidArgumentException e) {
+            e.printStackTrace();
+        } catch (OperationFailException e) {
+            e.printStackTrace();
+        }
     }
 }
